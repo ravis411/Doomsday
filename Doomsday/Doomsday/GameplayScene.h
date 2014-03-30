ed61a0df53b80180fa5dcfdddcfa25c72e1486af
Kyle Swinney <kmswinney@gmail.com>
2014-03-30 16:47:29 -0700

    GameplayScene
    Added but not yet implemented

---
+++ Doomsday/Doomsday/GameplayScene.h
@@ -0,0 +1,13 @@
+//
+//  GameplayScene.h
+//  Doomsday
+//
+//  Created by Kyle on 3/30/14.
+//  Copyright (c) 2014 TeamDoomsday. All rights reserved.
+//
+
+#import "CCScene.h"
+
+@interface GameplayScene : CCScene
+
+@end
---
+++ Doomsday/Doomsday/GameplayScene.m
@@ -0,0 +1,13 @@
+//
+//  GameplayScene.m
+//  Doomsday
+//
+//  Created by Kyle on 3/30/14.
+//  Copyright (c) 2014 TeamDoomsday. All rights reserved.
+//
+
+#import "GameplayScene.h"
+
+@implementation GameplayScene
+
+@end
