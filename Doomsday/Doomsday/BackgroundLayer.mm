a186679aaeb4015339669cdf5d14eba181487b6e
Kyle Swinney <kmswinney@gmail.com>
2014-03-30 17:43:14 -0700

    Placeholder sprites
    +Added placeholder sprites
    +Added attack layer
    -Removed deprecated layer implementation files

--- Doomsday/Doomsday/BackgroundLayer.mm
+++ Doomsday/Doomsday/BackgroundLayer.mm
@@ -17,6 +17,7 @@
         CGSize size = [[CCDirector sharedDirector] winSize];
         CCSprite *sky = [CCSprite node];
         [sky setTextureRectInPixels:CGRectMake(0,0,size.width,size.height)];
+        [self addChild:sky];
         
         distanceTraveled = 0;
     }
