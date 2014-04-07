283cbe8d7c5ce17ce7b8829c1d4856d37406a89e
Eric Liu <liueric@usc.edu>
2014-04-07 01:08:24 -0700

    merged with master

--- Doomsday/Doomsday/SpriteLayer.mm
+++ Doomsday/Doomsday/SpriteLayer.mm
@@ -45,7 +45,8 @@
         //Creating Edges around the screen
         b2BodyDef groundBodyDef;
         
-        groundBodyDef.position.Set(0,5.00000018);
+        //groundBodyDef.position.Set(0,5.00000018);
+        groundBodyDef.position.Set(0,0);
         
         b2Body *groundBody = _world->CreateBody(&groundBodyDef);
         b2EdgeShape groundEdge;
