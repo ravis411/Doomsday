7d0ca5e410c5779524c1e0f7a1b8da8046b0841b
Kyle Swinney <kmswinney@gmail.com>
2014-04-26 17:09:05 -0700

    Revert f960196..9610172
    This rolls back to commit f960196e2d100fe6a0da674db8d832c8674b8fda.

---
+++ Doomsday/Doomsday/MainMainScene.mm
@@ -0,0 +1,13 @@
+//
+//  MainMainScene.m
+//  Doomsday
+//
+//  Created by Kyle on 4/25/14.
+//  Copyright (c) 2014 TeamDoomsday. All rights reserved.
+//
+
+#import "MainMainScene.h"
+
+@implementation MainMainScene
+
+@end
